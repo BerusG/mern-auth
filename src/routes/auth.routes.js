db1d6b7702bd76be97ca592a1df5e422ec1ef77d
BerusG <maicolbenavidez124@gmail.com>
2023-06-15 14:29:30 -0500

    Final back-end

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,13 +5,15 @@ import {
   logout,
   profile,
 } from "../controllers/auth.controller.js";
+import { validateSchema } from "../middlewares/validatorMiddleware.js";
 
 import { authRequired } from "../middlewares/validateToken.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 // Post Routes
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 
 // Get Routes
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,14 +7,20 @@ import {
   updateTasks,
   deleteTasks,
 } from "../controllers/tasks.controller.js";
-
+import { validateSchema } from "../middlewares/validatorMiddleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 const router = Router();
 
 //Get routes
 router.get("/tasks", authRequired, getTasks);
 router.get("/tasks/:id", authRequired, getTask);
 //Post routes
-router.post("/tasks", authRequired, createTasks);
+router.post(
+  "/tasks",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTasks
+);
 //Delete routes
 router.delete("/tasks/:id", authRequired, deleteTasks);
 //Put routes
